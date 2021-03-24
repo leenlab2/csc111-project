18188e5ae92c87791df40c583223d33ae5d7ee39
leenlab2 <74102544+leenlab2@users.noreply.github.com>
2021-03-25 01:47:49 +0400

    Add files via upload

--- input.py
+++ input.py
@@ -4,14 +4,54 @@ locations they want to visit.
 
 This file is Copyright (c) 2021 Leen Al Lababidi, Michael Rubenstein, Maria Becerra and Nada Eldin
 """
-# TODO: import tkinter or another library
+import tkinter
+import datetime
+from typing import Union
+# TODO: import libraries
 
-# TODO: write a function that takes in input
 
-# TODO: write a function that saves that input somewhere/ something that can be called in main.py
-# you dont need to make a data storage thing, we can call the function in main.py and store it
-# in a variable
+def get_user_input() -> dict[str: Union[str, tuple[datetime.datetime, datetime.datetime],
+                                        datetime.time, list]]:
+    """Return a dictionary containing data inputted by the user, using a graphical user interface
+    implemented using tkinter.
 
-# uncomment this when you want
+    The returned dictionary will contain the keys 'hotel', 'days', 'leave', 'return', 'locations'.
+        - 'hotel' contains the <name> of the hotel the user is staying at. It is selected from a
+            drop-down menu of a few select hotels from the dataset
+        - 'days' is the day range that the user is visiting for. It is a tuple of arrival and
+            departure times.
+        - 'leave' is the time the user prefers to leave the hotel. It is a datetime object referring
+            to a particular hour in the day.
+        - 'return' is the time the user prefers to return to the hotel. It is a datetime object
+            referring to a particular hour in the day. It should be after 'leave' time.
+        - 'locations' contains a list of <names> of locations the user wants to visit. It is
+            optional, and if the user does not enter anything it is an empty list.
+    """
+    # TODO: write a function that takes in input
+    # feel free to make helper functions as necessary
+
+    return {
+        'hotel': ...,
+        'leave': ...,
+        'return': ...,
+        'locations': ...
+    }
+
+
+# TODO: uncomment this to check your code
 # if __name__ == "__main__":
-# TODO: check code with py_ta and doctests
+#     import doctest
+#     doctest.testmod()
+#
+#     import python_ta
+#     python_ta.check_all(config={
+#         'extra-imports': ['tkinter', 'datetime', 'python_ta.contracts'],
+#         'max-line-length': 100,
+#         'disable': ['R1705', 'C0200']
+#     })
+#
+#     # honestly i have no idea how to configure py_ta i forgot
+#
+#     import python_ta.contracts
+#     python_ta.contracts.DEBUG_CONTRACTS = False
+#     python_ta.contracts.check_all_contracts()
