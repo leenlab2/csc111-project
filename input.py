237df275fed243d400c6e998303846717fde0ef6
leenlab2 <74102544+leenlab2@users.noreply.github.com>
2021-04-15 01:26:42 +0400

    missing input field
    Changes:
    - moved code that runs the input fields to the "name == main" part, because that's where it should be
    - added TODO comments based on python_TA's complaints
    - changed the dict variable to input_dict because that is a keyword
    - added TODO comments to add the day_range input field

--- input.py
+++ input.py
@@ -4,26 +4,28 @@ locations they want to visit.
 
 This file is Copyright (c) 2021 Leen Al Lababidi, Michael Rubenstein, Maria Becerra and Nada Eldin
 """
-from tkinter import *
+from tkinter import *  # FIXME: it's better not to use the asterisk, just import tkinter -PyTA
 from tkinter.ttk import Combobox
 import datetime
-from typing import Union
 
 
 class PopUp:
     """ A Tk object used to preset the windows popup settings, display and functions.
     """
+    # FIXME: PyTA is complaining that you did not annotate the instance attributes. I'm not sure
+    #  if these are instance attributes. It's also complaining that there are too many.
 
-    def __init__(self, win: Tk, hotel_options: tuple):
+    def __init__(self, win: Tk, hotel_options: tuple):  # TODO add return type annotation
         self.lbl1 = Label(win, text='Hotel')
         self.lbl2 = Label(win, text='Leave time')
         self.lbl3 = Label(win, text='Return time')
         self.lbl4 = Label(win, text='Locations')
+        # TODO: add day range
 
         #  UI Scroll down options for hotel
         self.var = StringVar()
         self.var.set("one")
-        self.data = hotel_options
+        self.data = hotel_options  # TODO: do we actually have hotel data points??
         self.cb = Combobox(window, values=self.data)
         self.cb.place(x=200, y=50)
 
@@ -45,8 +47,7 @@ class PopUp:
         self.b1 = Button(win, text='Add', command=self.get_user_input)
         self.b1.place(x=150, y=250)
 
-    def get_user_input(self) -> dict[str: Union[str, tuple[datetime.datetime, datetime.datetime],
-                                                datetime.time, list]]:
+    def get_user_input(self) -> dict:
         """Return a dictionary containing data inputted by the user, using a graphical user
         interface implemented using tkinter.
         The returned dictionary will contain the keys 'hotel', 'days', 'leave', 'return',
@@ -64,48 +65,44 @@ class PopUp:
         """
         hotel = self.cb.get()
         leave_raw = self.t1.get()
+        # FIXME: leave and return refer to hours of the day the user wants to be outside
         dt_tuple_leave = tuple([int(x) for x in leave_raw[:10].split('-')]) + tuple(
             [int(x) for x in leave_raw[11:].split(':')])
-        datetimeobj1 = datetime.datetime(*dt_tuple_leave)
+        datetimeobj1 = datetime.datetime(*dt_tuple_leave)  # FIXME: should be datetime.time
 
         returns_raw = self.t2.get()
         dt_tuple_return = tuple([int(x) for x in returns_raw[:10].split('-')]) + tuple(
             [int(x) for x in returns_raw[11:].split(':')])
-        datetimeobj2 = datetime.datetime(*dt_tuple_return)
+        datetimeobj2 = datetime.datetime(*dt_tuple_return)  # FIXME: should be datetime.time
 
         locations_raw = self.t3.get()
         loc = locations_raw.split()
 
-        dict = {
+        # TODO: add day range
+
+        input_dict = {
             'hotel': hotel,
             'leave': datetimeobj1,
             'return': datetimeobj2,
             'locations': loc
         }
-        print(dict)
-        return dict
+        print(input_dict)
+        return input_dict
 
 
-window = Tk()
-mywin = PopUp(window, ("one", "two", "three", "four"))
-window.title('User Input Settings')
-window.geometry("400x300+10+10")
-window.mainloop()
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
-# TODO: uncomment this to check your code
-# if __name__ == "__main__":
-#     import doctest
-#     doctest.testmod()
-#
-#     import python_ta
-#     python_ta.check_all(config={
-#         'extra-imports': ['tkinter', 'datetime', 'python_ta.contracts'],
-#         'max-line-length': 100,
-#         'disable': ['R1705', 'C0200']
-#     })
-#
-#     # honestly i have no idea how to configure py_ta i forgot
-#
-#     import python_ta.contracts
-#     python_ta.contracts.DEBUG_CONTRACTS = False
-#     python_ta.contracts.check_all_contracts()
+    import python_ta
+    python_ta.check_all(config={
+        'extra-imports': ['tkinter', 'tkinter.ttk', 'datetime', 'python_ta.contracts'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
+
+    window = Tk()
+    mywin = PopUp(window, ("one", "two", "three", "four"))
+    window.title('User Input Settings')
+    window.geometry("400x300+10+10")
+    window.mainloop()
