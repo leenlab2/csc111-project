e5ee77c0d6f70cf728b194918c1656abda373057
leenlab2 <74102544+leenlab2@users.noreply.github.com>
2021-04-15 20:25:11 +0400

    added print statements

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import csv
 
 if __name__ == "__main__":
     # get hotels
+    print('Retrieving hotels....')
     hotels = {}
     with open('data/paris-hotel.csv') as hotel_file:
         hotel_reader = csv.reader(hotel_file)
@@ -22,31 +23,39 @@ if __name__ == "__main__":
             hotels[row[0]] = new_hotel
 
     # user input
+    print('Collecting user input....')
     hotel_names = tuple(hotels.keys())
     win_popup = input.open_input_window(hotel_names)
     user_input = win_popup.user_input
 
     # process input
+    print('Processing user input....')
     chosen_hotel = hotels[user_input['hotel']]
     planned_activities = user_input['locations']
     leave = user_input['leave']
     return_time = user_input['return']
 
     # load graphs
+    print('Loading city graph....')
     city_graph = graphs.load_city_graph('data/paris-attraction-final.csv',
                                         'data/paris-restaurant-organized-final.csv',
                                         'data/paris_metro_stations.csv', chosen_hotel)
+    print('Loading subway stations graph....')
     subway_graph = graphs.load_subway_graph('data/paris_metro_stations.csv',
                                             'data/paris_metro_lines.csv')
 
     # choose trip locations
+    print('Choosing locations to visit....')
     chosen_locations = choose_locations.choose_locations(city_graph, chosen_hotel,
                                                          planned_activities, leave, return_time)
 
     # find path
+    print('Finding route....')
     path = find_path.find_path(chosen_locations, city_graph, subway_graph)
 
     # get schedule
+    print('Building schedule....')
     schedule = schedule.build_schedule(path, leave, return_time)
 
     # TODO output
+    print('Displaying output....')
