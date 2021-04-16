0bd584134ce8b2ec53c03c2ac22c41e8bbf45e24
leenlab2 <74102544+leenlab2@users.noreply.github.com>
2021-04-16 21:06:13 +0400

    Update attraction_cleaner.py

--- attraction_cleaner.py
+++ attraction_cleaner.py
@@ -1,4 +1,6 @@
-"""Cleans attractions file"""
+"""This module contains functions that sort through the tourpedia attractions dataset in order to compile our final attractions dataset.
+This file is Copyright (c) 2021 Leen Al Lababidi, Michael Rubenstein, Maria Becerra and Nada Eldin
+"""
 import csv
 import requests
 
